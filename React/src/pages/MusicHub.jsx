2b5f46cc38fdb22cad2362075095376e0c2730b9
devalChovatia <deval.chovatia@gmail.com>
2024-08-14 17:36:32 -0400

    Fixes to 15inch

--- React/src/pages/MusicHub.jsx
+++ React/src/pages/MusicHub.jsx
@@ -66,7 +66,7 @@ export default function MusicHub() {
                 <Request />
             </div>    
          
-            <div className="mt-5 mr-[200px] md:ml-[400px] md:w-[500px] md:-mt-48 lg:ml-[650px] xl:ml-[1100px] 2xl:ml-[1550px]">
+            <div className="mt-5 mr-[200px] md:ml-[400px] md:w-[500px] md:-mt-48 lg:ml-[650px] xl:ml-[1100px] 2xl:ml-[1250px]">
                     <Links />
             </div>
             <div className='flex gap-20 md:gap-[250px] lg:gap-[330px] xl:gap-[550px] 2xl:gap-[740px] items-center md:mb-6 '>
