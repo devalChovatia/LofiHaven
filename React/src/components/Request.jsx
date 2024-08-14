6ec86bbab9818b990ddfa2110ddedaefc657e115
devalChovatia <deval.chovatia@gmail.com>
2024-08-14 18:03:53 -0400

    FINALLLLLLL

--- React/src/components/Request.jsx
+++ React/src/components/Request.jsx
@@ -12,7 +12,7 @@ export default function Request() {
         className="text-white font-sawarabi border p-1 md:mt-[30px] 
                     lg:mt-[30px] xl:mt-[40px] 2xl:mt-[45px]
                     rounded-3xl hover:bg-white hover:bg-opacity-20
-                    text-sm w-[160px] md:w-[200px] lg:w-[220px] 2xl:w-[240px] md:text-lg lg:text-lg xl:text-xl 2xl:text-2xl "
+                    text-sm w-[160px] md:w-[200px] lg:w-[220px] 2xl:w-[260px] md:text-lg lg:text-lg xl:text-xl 2xl:text-2xl "
                 >
         Submit a Request
       </button>
