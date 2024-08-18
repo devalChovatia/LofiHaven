914071b4f1ef47854ce702d4cee0b3a44d888d77
devalChovatia <deval.chovatia@gmail.com>
2024-08-18 19:32:11 -0400

    Complete Youtube API to Database Route

--- FastAPI/routers/youtube.py
+++ FastAPI/routers/youtube.py
@@ -1,13 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Query, HTTPException
 from googleapiclient.discovery import build
 from starlette import status
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from pydantic import BaseModel, Field
+from models import Livestream as DBLivestream
 
 router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally: 
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+class Livestream(BaseModel):
+    channel_name: str = Field(min_length=3, max_length=20)
+    livestream_name: str = Field(min_length=3, max_length=100)
+    livestream_link: str
+    genre_id: int = Field(gt=0)
 
 API_KEY = 'AIzaSyDuaSh0aE3hfJoAUWlPhRnKCKziG8VWemE'
 
 @router.get('/video', status_code=status.HTTP_200_OK)
-async def getVideo(url:str):
+async def getVideo(db:db_dependency, url:str, genre_id: int = Query(gt=0)):
     youtube = build('youtube', 'v3', developerKey=API_KEY)
     video_id = url.split('v=')[1]
     video_response = youtube.videos().list(
@@ -15,12 +36,28 @@ async def getVideo(url:str):
         id = video_id
     ).execute()
 
-    title = video_response['items'][0]['snippet']['title']
-    channelName = video_response['items'][0]['snippet']['channelTitle']
+    livestream_name = video_response['items'][0]['snippet']['title']
+    channel_name = video_response['items'][0]['snippet']['channelTitle']
+    livestream_link = f"https://www.youtube.com/watch?v={video_id}"
+
+    await storeLivestreamToDB(db, livestream_name, channel_name, livestream_link, genre_id)
 
     return {
-        'title': title,
-        'channleName': channelName
+        'livestream_name': livestream_name,
+        'channel_name': channel_name,
+        'livestream_link': livestream_link,
+        'genre_id': genre_id
     }
 
+async def storeLivestreamToDB(db: Session, livestream_name: str, channel_name: str, livestream_link: str, genre_id: int):
+    try: 
+        livestream_model = DBLivestream(
+            livestream_name=livestream_name,
+            channel_name=channel_name,
+            livestream_link=livestream_link, 
+            genre_id=genre_id)
     
+        db.add(livestream_model)
+        db.commit()
+    except Exception as e:
+        raise HTTPException(status_code=404, detail='Error In Updating Database')
